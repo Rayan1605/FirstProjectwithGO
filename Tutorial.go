2dfed398140998514309dfc4d5d5bcfec72181d9
rayan <rayanzyad99@gmail.com>
2024-02-22 10:29:26 +0000

    Start

--- Tutorial.go
+++ Tutorial.go
@@ -4,10 +4,15 @@ import "fmt"
 
 // To create a function just use func
 func main() {
-
 	fmt.Println("Welcome to the quiz game")
+	/*
 
-	var name = "Tim" // You can use name := Tim
-	name = "Hello"
-	fmt.Println(name)
+
+		var name = "Tim" // You can use name := Tim
+		name = "Rayan"
+		fmt.Println("Hello "+name+", How are you", name) // To add it in a print statement
+	*/
+	var name string
+	fmt.Scan(&name)
+
 }

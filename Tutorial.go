506fab81e13f9c29c2ceb11b11ab170a37eba6cd
rayan <rayanzyad99@gmail.com>
2024-02-22 11:11:01 +0000

    Start

--- Tutorial.go
+++ Tutorial.go
@@ -13,6 +13,35 @@ func main() {
 		fmt.Println("Hello "+name+", How are you", name) // To add it in a print statement
 	*/
 	var name string
+	fmt.Println("Enter your name: R")
 	fmt.Scan(&name)
+	fmt.Println("")
+	fmt.Println("Hello " + name + ", welcome to the game!")
 
+	fmt.Println("Enter your age")
+
+	var age uint
+	fmt.Scan(&age)
+
+	// To make an if loop
+	if age >= 10 {
+		fmt.Println("You are the correct age to play")
+	} else {
+		fmt.Println("You cannot play")
+	}
+	fmt.Println("Quiz is on Geography")
+
+	option := map[string]string{
+		"Algeria":      "Morocco", // Also, there was a typo in "Morocco".
+		"Italy":        "England",
+		"Ireland":      "India",
+		"Egypt":        "Russia",
+		"South Africa": "Sudan",
+	}
+
+	fmt.Println("")
+	for option1, option2 := range option {
+
+		fmt.Println(" Which one is bigger")
+	}
 }

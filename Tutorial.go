345d90de48c7e0f223b48790caaa487ee68cea62
rayan <rayanzyad99@gmail.com>
2024-02-22 10:20:28 +0000

    Start

--- Tutorial.go
+++ Tutorial.go
@@ -1,11 +1,12 @@
 package main
 
 import "fmt"
+
 // To create a function just use func
 func main() {
 
 	fmt.Println("Welcome to the quiz game")
 
-	var name float64 =
+	var name string = "Tim"
 
 }
